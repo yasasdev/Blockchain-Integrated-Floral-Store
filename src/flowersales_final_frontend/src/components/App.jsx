8ea6d107659ac851f7bd9e362e62a72145df40a8
yasasdev <yasaslekamge@gmail.com>
2025-03-02 11:46:06 +0530

    Header and Footer files added.

--- src/flowersales_final_frontend/src/components/App.jsx
+++ src/flowersales_final_frontend/src/components/App.jsx
@@ -1,6 +1,14 @@
 import { flowersales_final_backend } from 'declarations/flowersales_final_backend';
+import Header from './Header';
+import Footer from './Footer';
 
 function App() {
+    return(
+        <div>
+            <Header />
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
---
+++ src/flowersales_final_frontend/src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+
+function Header() {
+
+    return(
+        <div></div>
+    );
+}
+
+export default Header
