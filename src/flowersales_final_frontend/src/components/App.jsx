d045e183556a1a529145a8f5a2da33bdfe04f24a
yasasdev <yasaslekamge@gmail.com>
2025-03-02 15:17:25 +0530

    Multiple files update.

--- src/flowersales_final_frontend/src/components/App.jsx
+++ src/flowersales_final_frontend/src/components/App.jsx
@@ -1,11 +1,13 @@
 import { flowersales_final_backend } from 'declarations/flowersales_final_backend';
 import Header from './Header';
 import Footer from './Footer';
+import About from './About';
 
 function App() {
     return(
         <div>
             <Header />
+            <About />
             <Footer />
         </div>
     );
